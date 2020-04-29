05bb6d848456a5416c87bd1b975962e494f6083a
Vladimir Vasilenko <vladimir.a.vasilenko@gmail.com>
2020-04-29 17:36:48 +0900

    added check even function

---
+++ brain_games/even_check.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+"""Function to check whether numbe is even or not."""
+"""Return true if even, return false is uneven."""
+
+def check_if_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
