0b78c441eebc09c121a9ccc159ab9d0e5d3c6000
Vladimir Vasilenko <vladimir.a.vasilenko@gmail.com>
2020-05-01 00:10:59 +0900

    fixed improper import in brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,9 +2,10 @@
 
 """Greeting messages and users representation."""
 
+import sys
+sys.path.insert(1, '/home/alabarym/git/alabarym/python-project-lvl1/brain_games')
 import greet_first
 import cli
-import sys
 
 sys.path.insert(1, '/home/alabarym/git/alabarym/python-project-lvl1/brain_games')
 
@@ -13,7 +14,6 @@ def main():
     """Script to say hi to user."""
     greet_first.greet_first()
     cli.welcome_user()
-
 
 if __name__ == '__main__':
     main()
