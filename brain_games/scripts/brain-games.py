45737569e5ca00e050426fdaa35423577c2090ee
Vladimir Vasilenko <vladimir.a.vasilenko@gmail.com>
2020-04-30 09:42:58 +0900

    fixed Makefile build problems

--- brain_games/scripts/brain-games.py
+++ brain_games/scripts/brain-games.py
@@ -1,17 +1,17 @@
-# -*- coding:utf-8 -*-
+# # -*- coding:utf-8 -*-
 
-"""Greeting messages and users representation."""
+# """Greeting messages and users representation."""
 
-import sys
+# import sys
 
-sys.path.insert(
-    1, '/home/alabarym/git/alabarym/python-project-lvl1/brain_games')
-import cli
-import greet_first
+# sys.path.insert(
+#     1, '/home/alabarym/git/alabarym/python-project-lvl1/brain_games')
+# import cli
+# import greet_first
 
-def main():
-    greet_first.greet_first()
-    cli.welcome_user()
+# def main():
+#     greet_first.greet_first()
+#     cli.welcome_user()
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,19 +1,19 @@
-# # -*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
-# """Greeting messages and users representation."""
+"""Greeting messages and users representation."""
 
-# import greet_first
-# import cli
-# import sys
+import greet_first
+import cli
+import sys
 
-# sys.path.insert(
-#     1, '/home/alabarym/git/alabarym/python-project-lvl1/brain_games')
-
-
-# def main():
-#     greet_first.greet_first()
-#     cli.welcome_user()
+sys.path.insert(
+    1, '/home/alabarym/git/alabarym/python-project-lvl1/brain_games')
 
 
-# if __name__ == '__main__':
-#     main()
+def main():
+    greet_first.greet_first()
+    cli.welcome_user()
+
+
+if __name__ == '__main__':
+    main()
