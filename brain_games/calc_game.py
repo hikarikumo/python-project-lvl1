3e32c648beae8c884d3771b6c4e8e3dcd58b45a8
Vladimir Vasilenko <vladimir.a.vasilenko@gmail.com>
2020-05-15 15:44:30 +0300

    trying to solve CodClimte complexity for calc_game

--- brain_games/calc_game.py
+++ brain_games/calc_game.py
@@ -13,10 +13,10 @@ def calc_greet_first():
 
 def calc_game_summ():
     """Summary operation."""
-    random_number1 = random.randint(0, 100)
-    random_number2 = random.randint(0, 100)
-    summ_result = random_number1 + random_number2
-    return summ_result, random_number1, random_number2
+    number1 = random.randint(0, 100)
+    number2 = random.randint(0, 100)
+    summ_result = number1 + number2
+    return summ_result, number1, number2
 
 
 def summ_check():
@@ -33,41 +33,41 @@ def summ_check():
 
 def calc_game_subtract():
     """Subtract operation."""
-    random_number3 = random.randint(0, 100)
-    random_number4 = random.randint(0, 100)
-    answer_subtraction = random_number3 - random_number4
-    return answer_subtraction, random_number3, random_number4
+    number3 = random.randint(0, 100)
+    number4 = random.randint(0, 100)
+    answer_subtr = number3 - number4
+    return answer_subtr, number3, number4
 
 
 def subtract_check():
     """Check calculation of substract by user."""
-    answer_subtraction, random_number3, random_number4 = calc_game_subtract()
-    question_subtraction = prompt.string('Question: ' + str(random_number3) + ' - ' + str(random_number4) + '\n')
-    if question_subtraction == str(answer_subtraction):
-        print('Your answer: ' + question_subtraction)
+    answer_subtr, number3, number4 = calc_game_subtract()
+    question_subtr = prompt.string('Question: ' + str(number3) + ' - ' + str(number4) + '\n')
+    if question_subtr == str(answer_subtr):
+        print('Your answer: ' + question_subtr)
         print('Correct!')
         return True
-    print("'" + str(question_subtraction) + "' " + "is wrong answer ;(. Correct answer was " + "'" + str(answer_subtraction) + "'")
+    print("'" + str(question_subtr) + "' " + "is wrong answer ;(. Correct answer was " + "'" + str(answer_subtr) + "'")
     return False
 
 
 def calc_game_multiplication():
     """Multiplication operation."""
-    random_number5 = random.randint(0, 10)
-    random_number6 = random.randint(0, 10)
-    answer_multiplication = random_number5 * random_number6
-    return answer_multiplication, random_number5, random_number6
+    number5 = random.randint(0, 10)
+    number6 = random.randint(0, 10)
+    answer_mult = number5 * number6
+    return answer_mult, number5, number6
 
 
 def multiplication_check():
     """Check calculation of multiplication by user."""
-    answer_multiplication, random_number5, random_number6 = calc_game_multiplication()
-    question_multiplication = prompt.string('Question: ' + str(random_number5) + ' * ' + str(random_number6) + '\n')
-    if question_multiplication == str(answer_multiplication):
-        print('Your answer: ' + question_multiplication)
+    answer_mult, number5, number6 = calc_game_multiplication()
+    question_mult = prompt.string('Question: ' + str(number5) + ' * ' + str(number6) + '\n')
+    if question_mult == str(answer_mult):
+        print('Your answer: ' + question_mult)
         print('Correct!')
         return True
-    print("'" + str(question_multiplication) + "' " + "is wrong answer ;(. Correct answer was " + "'" + str(answer_multiplication) + "'")
+    print("'" + str(question_mult) + "' " + "is wrong answer ;(. Correct answer was " + "'" + str(answer_mult) + "'")
     return False
 
 
