4aaa579aa85c77a6f1ce741efffe1216cc8d501f
hikarikumo <hikarikumo@svetozar.space>
2023-12-18 12:50:53 +0200

    testing github actions build error whether it is connected with imports
    Signed-off-by: hikarikumo <hikarikumo@svetozar.space>

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,7 +2,11 @@
 
 """Main cli module for even game."""
 
-import brain_games
+import sys
+import os
+current_script_dir = os.path.dirname(__file__)
+brain_games_path = os.path.join(current_script_dir, '..')
+sys.path.append(brain_games_path)
 from even_game import main
 
 
