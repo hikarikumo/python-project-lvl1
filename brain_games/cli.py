0bba722bf4ef4a054b976d396c093d80a487399e
hikarikumo <hikarikumo@svetozar.space>
2023-12-18 12:23:00 +0200

    modified name request semantics
    Signed-off-by: hikarikumo <hikarikumo@svetozar.space>

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -16,7 +16,7 @@ def welcome_user():
     Returns:
         name of the user.
     """
-    user_name = prompt.string('May I know your name? ')
+    user_name = prompt.string('May I have your name? ')
     print('Hello, ', user_name, '!', sep='')
     return user_name
 
